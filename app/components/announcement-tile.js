98dde94ed959dc017214589eac93bafd6adfd5fd
George Olson and Wolfgang Warneke <george+wolfgang@>
2016-08-16 10:17:30 -0700

    add CRUD stuff, dynamic routes, and page templates

--- app/components/announcement-tile.js
+++ app/components/announcement-tile.js
@@ -3,20 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   the_gosh_dang_truth: true,
   testFunction: function() { this.the_gosh_dang_truth = false; },
-  recentPronouncement: new function() {
-    return false;
-  },
   actions: {
-    makeFalse: function() {
-      // var todaysDate = new Date().toJSON;
-      // if ( announcementDate < todaysDate) {
-      //   return true;
-      // } else {
-      //   return false;
-      // }
-      
-    },
-
     // save1() {
     //   var params = {
     //     owner: this.get('owner') ? this.get('owner') : "",
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,20 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    var modelObject = {};
-    modelObject["rentals"] = this.store.findAll('rental');
-    modelObject["announcements"] = this.store.findAll('announcement');
-    return modelObject;
-  },
-
-  actions: {
+     return this.store.findAll('rental');
+   },
+   actions: {
     save3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
-      this.transitionTo('index');
-    },
-    destroyRental(rental) {
-      rental.destroyRecord();
       this.transitionTo('index');
     }
   }
